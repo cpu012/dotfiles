ce3da5a3c0d474def5a30d4fc270a68710265e7c
cpu012 <shloimy657@gmail.com>
2020-06-21 08:32:51 -0400

    update dmenu fonts

--- builds/dmenu/config.h
+++ builds/dmenu/config.h
@@ -5,7 +5,9 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
+      "Twemoji:size=10",
+      "Symbola:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
