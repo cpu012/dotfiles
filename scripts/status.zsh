7f04424334de7eec63d36b2628e7715c86500d47
cpu012 <shloimy657@gmail.com>
2020-08-16 11:07:50 -0400

    update status script (for dwm)

--- scripts/status.zsh
+++ scripts/status.zsh
@@ -1,12 +1,20 @@
 #!/bin/zsh
-# NOTE: this script is designed for dwm with the 2dstatus patch, will not work on most other wm/de, it does not work with the stdin patch.
-# NOTE: the battery part will not work on laptops with more than one battery
 # XXX: dependencies: acpi, zsh, pamixer, pulseaudio, networkmanager
 
 # config
 interval=1
+sep_sym=' | '
+
+# color config
+color_good="#66d16b"
+color_normal="#41b8f2"
+color_degraded="#007dae"
+color_bad="#fa2e28"
+color_unk="#cf8dd2"
+color_sep=$color_degraded
 
 
+sep='^c'$color_sep'^'$sep_sym'^d^'
 while [ true ]; do
     time=$(date +%Y\ %b\ \(%a.\)\ %I:%M:%S\ %p)  # get current time
 
@@ -16,17 +24,24 @@ while [ true ]; do
     mem_cmd=$(free -h)
     mem=${(z)${${mem_cmd#*Mem:}%Swap:*}}    # get ram usage
     mem_byte=${(z)${${mem_cmd_byte#*Mem:}%Swap:*}}    # get ram usage in bytes
-
     mem_used=$(echo $mem | cut -d' ' -f2)
     mem_available=$(echo $mem | cut -d' ' -f6)
     mem_total=$(echo $mem | cut -d' ' -f1)
-
     mem_used_byte=$(echo $mem_byte | cut -d' ' -f2)
     mem_total_byte=$(echo $mem_byte | cut -d' ' -f1)
-    
-    mem_per_used=$(($mem_used_byte * 100 / $mem_total_byte))'%'     # calculate ram used percentage
+    mem_per_used=$(($mem_used_byte * 100 / $mem_total_byte))     # calculate ram used percentage
 
-    mem_format=$mem_used' - '$mem_available' ('$mem_per_used')'
+    # define colors for displaying ram
+    if [ $mem_per_used -gt 85 ]; then
+        mem_color='^c'$color_degraded'^'
+    elif [ $mem_per_used -gt 50 ]; then
+        mem_color='^c'$color_bad'^'
+    elif [ $mem_per_used -le 50 ]; then
+        mem_color='^c'$color_normal'^'
+    else
+        mem_color='^c'$color_unk'^'
+    fi
+    mem_format=$mem_color$mem_used' - '$mem_available' ('$mem_per_used'%)'
 
 
     # battery
@@ -35,9 +50,32 @@ while [ true ]; do
     bat_state=${${${bat#*: }%, *}%,*}
     bat_per=${${bat#*, }%, *}
     bat_time=$(echo ${(z)${bat#*%, }} | cut -d' ' -f1)
-    bat_draw=$(echo - | awk "{printf \"%.1f\", $(( $(cat $bat_path'current_now') * $(cat $bat_path'voltage_now') )) / 1000000000000 }" ; echo "W")
+    bat_per_raw=${bat_per%'%'}
 
-    bat_format=$bat_state' '$bat_per' '$bat_time' ('$bat_draw')'
+    if [ $bat_per_raw -gt 40 ]; then
+        bat_color='^c'$color_normal'^'
+        if [ $bat_state = 'Charging' ]; then
+            bat_color='^c'$color_good'^'
+        elif [ $bat_state = 'Full' ]; then
+            bat_color='^c'$color_good'^'
+        fi
+    elif [ $bat_per_raw -le 25 ]; then
+        bat_color='^c'$color_bad'^'
+    elif [ $bat_per_raw -le 40 ]; then
+        bat_color='^c'$color_degraded'^'
+    else
+        bat_color='^c'$color_unk'^'
+    fi
+
+    # draw a batery image representing the battery percentage
+    if [ $bat_state = 'Charging' ]; then
+        bat_chrg_drw='⚡'
+    elif [ $bat_state = 'Full' ]; then
+        bat_chrg_drw='⚡'
+    fi
+    bat_drw=$bat_chrg_drw"^c#cccccc^^r0,7,2,4^^r2,4,22,10^^c#08090e^^r3,5,20,8^"$bat_color"^r"$(( 23 - $bat_per_raw / 5 ))",5,"$(( $bat_per_raw / 5 ))",8^^d^^f24^ "
+
+    bat_format=$bat_color$bat_drw$bat_per' '$bat_time
 
 
     # audio
@@ -55,17 +93,23 @@ while [ true ]; do
     load_cmd=$(uptime)
     load=${${load_cmd#*average: }//,/}
 
-    load_format=$load
+    load_minute=$(echo $load | cut -d' ' -f1)
+    load_minute_int=${load_minute//./}
+
+    if [ $load_minute_int -gt 80 ]; then
+        load_color='^c'$color_bad'^'
+    elif [ $load_minute_int -gt 60 ]; then
+        load_color='^c'$color_degraded'^'
+    elif [ $load_minute_int -lt 60 ]; then
+        load_color='^c'$color_normal'^'
+    else
+        load_color='^c'$color_unk'^'
+    fi
+
+    load_format=$load_color$load
 
 
-    # networking
-    net_connection=$(echo ${(z)$(nmcli c show)} | cut -d' ' -f5)
-    net_quality=$(echo ${(z)$(nmcli d wifi list | grep '*')} | cut -d' ' -f8)
-
-    net_format=$net_quality'% ('$net_connection')'
-
-
-    format=$mem_format' | '$bat_format' | '$net_format' | '$load_format' | '$audio_format' | '$time
+    format=$mem_format$sep$bat_format$sep$load_format$sep$audio_format$sep$time'^d^'
     xsetroot -name $format
     sleep $interval
 done
